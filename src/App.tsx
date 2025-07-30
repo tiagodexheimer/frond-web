898a12f737eaf56e8b6a4e04aa60fb31582b0a9d
tiagodexheimer <fenixpalwashan@gmail.com>
2025-07-30 13:45:05 -0300

    feat: logo 1 e 2

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        <img src="../public/frond-logo2.png"></img>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
